c22d2b4de7bda927e8de55b8621f90c158213f5d
roxyle <ylenia.rossi@outlook.com>
2024-07-22 10:17:30 +0200

    first commit for this angular18 testing project

---
+++ src/app/navBar/navbar.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-navbar',
+    standalone: true,
+    template: '<h1>navbar component</h1>'
+})
+
+export class AppNavbar {
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { AppNavbar } from './app/navBar/navbar.component'
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppNavbar, appConfig)
   .catch((err) => console.error(err));
